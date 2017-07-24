66739886e1b22e9a79f491a1a57df7802f612c23
Ciaran Crocker <crox@ciarancrocker.net>
2017-07-24 14:31:39 +0100

    Don't log when users are streaming

--- presence_handler.js
+++ presence_handler.js
@@ -4,7 +4,8 @@ const winston = require('winston');
 module.exports = async function(oldM, newM) {
   // if the game the member is playing has changed, update sessions accordingly
   if (!oldM.presence.equals(newM.presence)) {
-    if (oldM.presence.game != null && oldM.presence.game.name != null) {
+    if (oldM.presence.game != null && oldM.presence.game.name != null
+      && !oldM.presence.game.streaming) {
       // user finished session
       winston.log('info', 'User %s stopped playing %s', oldM.user.tag,
         oldM.presence.game.name);
@@ -12,7 +13,8 @@ module.exports = async function(oldM, newM) {
       const gameId = await database.findOrCreateGame(oldM.presence.game.name);
       await database.endSession(userId, gameId);
     }
-    if (newM.presence.game != null && newM.presence.game.name != null) {
+    if (newM.presence.game != null && newM.presence.game.name != null
+      && !newM.presence.game.streaming) {
       // user starting session
       winston.log('info', 'User %s started playing %s', newM.user.tag,
         newM.presence.game.name);
