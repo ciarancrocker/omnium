9ee021d3419755c4247fea1e090e7ceaf225b813
Isabel Jenkins <unreturnable@sucs.org>
2017-08-29 02:13:24 +0100

    Updated user_stats and game_graph to match game_stats

--- event_handlers/channel_handler.js
+++ event_handlers/channel_handler.js
@@ -111,6 +111,7 @@ async function provisionTemporaryChannels(guild) {
  * @param {GuildMember} after - Member after the update
  */
 function handleVoiceStateUpdate(before, after) {
+  console.log(ch);
   // do renaming
   if (before.voiceChannelID != null) {
     updateChannel(
