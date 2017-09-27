ab1a18f8a2f15c69bf1162dba79d547e6aaa8a2f
Ciaran Crocker <crox@ciarancrocker.net>
2017-09-27 14:07:42 +0100

    Use 120 characters as a limit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,17 @@
 module.exports = {
-    "extends": "google",
-  "parserOptions": {
-    "ecmaVersion": 2017,
+  extends: 'google',
+  parserOptions: {
+    ecmaVersion: 2017,
+  },
+  rules: {
+    "max-len": [    
+      2,
+      {                       
+        "code": 120,  
+        "tabWidth": 2,
+        "ignoreUrls": true,
+        "ignorePattern": "^goog.(module|require)"
+      }                    
+    ]
   },
 };
