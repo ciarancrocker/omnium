88fae0aae95bf772a83b11e5fb855a72907c1987
Ciaran Crocker <crox@ciarancrocker.net>
2018-01-16 20:25:00 +0000

    Fix presence type check
    Docs say it can be the word or a number. I'll assume for now that it's
    just numbers coming out of the API.

--- event_handlers/presence_handler.js
+++ event_handlers/presence_handler.js
@@ -9,14 +9,14 @@ module.exports = async function(oldM, newM) {
 
   // if the game the member is playing has changed, update sessions accordingly
   if (!oldM.presence.equals(newM.presence)) {
-    if (oldM.presence.game != null && oldM.presence.game.name != null && oldM.presence.game.type === 'PLAYING') {
+    if (oldM.presence.game != null && oldM.presence.game.name != null && oldM.presence.game.type === 0) {
       // user finished session
       winston.log('info', 'User %s stopped playing %s', oldM.user.tag, oldM.presence.game.name);
       const userId = await database.findOrCreateUser(oldM.user);
       const gameId = await database.findOrCreateGame(oldM.presence.game.name);
       await database.endSession(userId, gameId);
     }
-    if (newM.presence.game != null && newM.presence.game.name != null && newM.presence.game.type === 'PLAYING') {
+    if (newM.presence.game != null && newM.presence.game.name != null && newM.presence.game.type === 0) {
       // user starting session
       winston.log('info', 'User %s started playing %s', newM.user.tag, newM.presence.game.name);
       const userId = await database.findOrCreateUser(newM.user);
