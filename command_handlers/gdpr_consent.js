a9d4a3a445c83135e6a09a837b9ce0fdf7d70c25
Ciaran Crocker <crox@ciarancrocker.net>
2018-05-24 15:26:11 +0100

    Don't try delete the message unless we can

--- command_handlers/gdpr_consent.js
+++ command_handlers/gdpr_consent.js
@@ -5,7 +5,9 @@ module.exports = {
   handler: async function(message) {
     await db.pool.query('UPDATE users SET gdpr_consented=true WHERE discord_id=$1', [message.author.id]);
     await message.author.send('Your consent preference has been updated. Thank you!');
-    await message.delete();
+    if (message.channel.type == 'text') {
+      await message.delete();
+    }
   },
   help: 'Allow the bot to store and process your personal information',
 };
