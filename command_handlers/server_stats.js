2c8390d366e6a6873119fc55b4ee9dc01f82cb35
Ciaran Crocker <crox@ciarancrocker.net>
2018-09-11 13:07:51 +0100

    Add showing current stats to server_stats

--- command_handlers/server_stats.js
+++ command_handlers/server_stats.js
@@ -5,9 +5,15 @@ module.exports = {
   bind: 'server_stats',
   handler: async function(message) {
     const {rows: aggregateStats} = await db.pool.query('SELECT max(total_members) as max_members, max(online_members) as max_online_members FROM server_statistics');
+
+    const members = message.client.guilds.first().members.array();
+    const onlineMembers = members.filter((x) => x.presence.status != 'offline');
+
     const embed = new RichEmbed();
     embed.setTitle('Aggregate server statistics')
+      .addField('Current total members', members.length, true)
       .addField('Peak total members', aggregateStats[0].max_members, true)
+      .addField('Current online members', onlineMembers.length, true)
       .addField('Peak online members', aggregateStats[0].max_online_members, true)
       .setColor(0x004d00);
     await message.channel.send(embed);
