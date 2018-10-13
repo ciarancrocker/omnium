d9a2ded9feb8698a7a883e6df2ac6bf7f90ac5ab
Isabel Jenkins <unreturnable@users.noreply.github.com>
2018-10-13 22:35:06 +0100

    Random chance on ping

--- command_handlers/ping.js
+++ command_handlers/ping.js
@@ -2,7 +2,11 @@ if (process.env.FEAT_UTIL) {
   module.exports = {
     bind: 'ping',
     handler: async function(message) {
-      message.reply('pong');
+      if (Math.floor(Math.random() * 100) === 99) {
+        message.reply('nah fam');
+      } else {
+        message.reply('pong');
+      }
     },
     help: 'Ping-pong!',
   };
