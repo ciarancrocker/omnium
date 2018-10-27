353a1c24a44d93f4b8f18642bc24c54de5571722
Ciaran Crocker <crox@ciarancrocker.net>
2018-10-27 17:30:05 +0100

    Upgrade winston to 3.x

--- index.js
+++ index.js
@@ -2,30 +2,9 @@
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const winston = require('winston');
-
-require('./lib/winston-discord');
+const logger = require('./lib/logging');
 
 require('dotenv').config();
-
-// Configure logging to file and console
-// Developers can then just `require('winston')` to log in their modules
-winston.configure({
-  transports: [
-    new (winston.transports.Console)({
-      level: 'debug',
-      timestamp: true,
-    }),
-    new (winston.transports.File)({
-      filename: 'sgs_bot.log',
-    }),
-    new (winston.transports.DiscordLogger)({
-      client: client,
-      channelId: process.env.DISCORD_LOG_CHANNEL,
-      level: 'error',
-    }),
-  ],
-});
 
 // set up our event handlers
 const channelHandler = require('./event_handlers/channel_handler');
@@ -37,13 +16,12 @@ const memberHandlers = require('./event_handlers/member_handlers');
 const serverStatsTask = require('./lib/server_statistics');
 
 client.on('ready', async function() {
-  winston.log('info', 'Logged into Discord as %s', client.user.tag);
+  logger.log('info', 'Logged into Discord as %s', client.user.tag);
   // set a useful presence message
   await client.user.setActivity(`Ask me for ${process.env.COMMAND_PREFIX}help`);
-  winston.log('info', 'Using %s as prefix for commands',
-    process.env.COMMAND_PREFIX);
+  logger.log('info', 'Using %s as prefix for commands', process.env.COMMAND_PREFIX);
   // fire off an update for all channels in case they've gotten inconsistent
-  winston.log('info', 'Performing initial channel scan');
+  logger.log('info', 'Performing initial channel scan');
   client.guilds.array().forEach((guild) => channelHandler.updateChannelsForGuild(guild));
 
   // set up recurring functions now
@@ -60,11 +38,11 @@ client.on('guildMemberRemove', memberHandlers.memberLeave);
 
 // If something bad happens, let the process manager restart us properly
 client.on('disconnect', function(ev) {
-  winston.log('debug', 'Discord.js websocket disconnected, terminating.', ev.reason);
+  logger.log('debug', 'Discord.js websocket disconnected, terminating.', ev.reason);
   process.exit(1);
 });
 client.on('error', function(err) {
-  winston.log('debug', 'Discord.js client emitted error event, terminating.', err);
+  logger.log('debug', 'Discord.js client emitted error event, terminating.', err);
   process.exit(1);
 });
 
